5ea3b3d38d91949debd74c3941e398c94470baaa
Tommy Tran <haitran1995@gmail.com>
2022-09-06 00:38:01 -0400

    Filled out the urls_index template

--- express_server.js
+++ express_server.js
@@ -7,15 +7,25 @@ app.set('view engine', 'ejs');
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "9sm5xK": "http://www.google.com",
 }
 
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-app.get("/urls.json", (req, res) => {
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase} // < templateVars is now urls which = the urlDatabase obj
+  res.render("urls_index", templateVars);
+})
+
+app.get("/urls.json", (req, res) => { // ?why is 'json' there?
   res.json(urlDatabase); // < Shows urlDatabase obj on new site/page.
+});
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n"); // < World is bold
 });
 
 app.listen(PORT, () => {
