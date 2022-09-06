0059d83283e94585738ccf9a38545f492836d966
Tommy Tran <haitran1995@gmail.com>
2022-09-05 23:12:39 -0400

    Managed to get 'Hello' showing on chrome

---
+++ express_server.js
@@ -0,0 +1,19 @@
+// Express is our web framework for our server
+const express = require("express"); 
+const app = express();
+const PORT = 8080;
+
+app.set('view engine', 'ejs');
+
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+}
+
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listeing on port ${PORT}`);
+});
