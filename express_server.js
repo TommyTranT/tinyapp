232a87c6c8e022f3e43d771b0debc9337369f1a4
Tommy Tran <haitran1995@gmail.com>
2022-09-06 02:58:55 -0400

    Added the GET and POST for submit new form

--- express_server.js
+++ express_server.js
@@ -3,13 +3,19 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
+// Requirements
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+
+// Data
+function generateRandomString() {}
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 }
 
+// Routes
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -27,10 +33,21 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 })
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]}; // < longUrl = objName[ID but with the req.params before it]
   res.render("urls_show", templateVars);
 });
+
+// Post
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
 
 // Listening
 app.listen(PORT, () => {
